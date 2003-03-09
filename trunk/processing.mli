fdb12f7490108e85a5a47669e1ced53fed096942
xleroy <xleroy@ff4f2081-346c-4227-9053-d32350c8aa95>
2003-03-09 15:12:18 +0000

    Ajout commande stat

--- trunk/processing.mli
+++ trunk/processing.mli
@@ -18,3 +18,5 @@ val mark_message : Database.short -> string -> unit
 val record_words : Database.full -> bool -> string -> unit
 val add_message : Database.full -> bool -> bool -> string -> unit
 val test_message : Database.short -> float -> float -> string -> unit
+type message_class = Msg_good | Msg_unknown | Msg_spam
+val stat_message : Database.short -> string -> message_class
